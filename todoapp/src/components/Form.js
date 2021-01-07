b7b0ea2b9a7adfaf3c14a0b322f8bc87937bf877
Dimitar <dimitar.georgiev159@gmail.com>
2021-01-07 22:58:20 +0200

    Finished

---
+++ todoapp/src/components/Form.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import nextId from "react-id-generator";
+
+function Form({ setInputText, todos, setTodos, inputText, setStatus }){
+  function inputTextHandler(e){
+    setInputText(e.target.value);
+  }
+  function submitTodoHandler(e){
+    e.preventDefault();
+    setTodos([
+      ...todos, {text: inputText, completed: false, id: nextId() }
+    ]);
+    setInputText("");
+  }
+  function statusHandler(e){
+    setStatus(e.target.value)
+  }
+  return(
+    <form>
+      <input value={inputText} 
+      onChange={inputTextHandler} 
+      type="text" 
+      className="todo-input" 
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+    );
+  }
+
+export default Form;
